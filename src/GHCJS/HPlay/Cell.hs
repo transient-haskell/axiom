fff030604a28d3551718857b792fbedc81d1cfe9
Alberto G. Corona <agocorona@gmail.com>
2016-09-28 17:12:18 +0200

    cell improvement

--- src/GHCJS/HPlay/Cell.hs
+++ src/GHCJS/HPlay/Cell.hs
@@ -13,16 +13,16 @@
 -----------------------------------------------------------------------------
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverloadedStrings, CPP, ScopedTypeVariables #-}
 module GHCJS.HPlay.Cell(Cell(..),boxCell,(.=),get,mkscell,scell, gcell, calc)  where
-import Transient.Base hiding((<**))
+import Transient.Base
 import Transient.Move
-import qualified Transient.Internals as Internals
-
+import Transient.Internals (runTransState)
+import Transient.Backtrack
 import GHCJS.HPlay.View
 import Data.Typeable
 import Unsafe.Coerce
 import qualified Data.Map as M hiding ((!))
 import System.IO.Unsafe
-import Data.IORef
+
 import Control.Monad.IO.Class
 import Control.Monad
 import Data.Maybe
@@ -60,16 +60,16 @@ boxCell id = Cell{ mk= \mv -> getParam  (Just id) "text" mv
                             Just e ->  setProp e "value" (toJSString $ show1 x)
                             Nothing -> return ()
 
-                 , getter= getit id}
+                 , getter= getID id}
 
-getit id = withElem id $ \e -> do
+getID id = withElem id $ \e -> do
   ms <- getValue e
   case ms of
     Nothing -> return Nothing
     Just s  -> return $ read1  s
   where
   read1 s=
-      if typeOf(typeIO getit) /= typestring
+      if typeOf(typeIO getID) /= typestring
            then case readsPrec 0  s  of
                [(v,_)] -> v `seq` Just v
                _       -> Nothing
@@ -134,19 +134,18 @@ infixr 0 .=  -- , ..=
 -- see http://tryplayg.herokuapp.com/try/spreadsheet.hs/edit
 gcell ::   JSString -> Widget Double
 gcell n= Widget $ do
-  vars <- liftIO $ readIORef rvars
-  case M.lookup n vars  of
-    Just exp -> inc n  exp
-    Nothing -> error $ "cell not found: "++ show n
+  Vars vars <- getSData <|> return(Vars M.empty ) -- liftIO $ readIORef rvars
+  case M.lookup n vars !> M.size vars of
+    Just exp -> do inc n  exp;  exp
+    Nothing -> error $ "cell not found: " ++ show n
   where
-  inc n exp= unsafePerformIO $ do
-     tries <- readIORef rtries
+  inc n exp=  do
+     Tries tries maxtries<- getSData <|> do
+                                      Exprs exprs <- getSData
+                                      return . Tries 0 $ 3 * (M.size $  exprs)
      if tries <= maxtries
-       then  do
-          writeIORef rtries  (tries+1)
-          return exp
-
-       else  throw Loop
+       then  setData $ Tries (tries+1) maxtries
+       else  back Loop
 
 data Loop= Loop deriving (Show,Typeable)
 
@@ -155,19 +154,23 @@ instance Exception Loop
 -- a parameter is a function of all of the rest
 type Expr a = TransIO a
 
-rtries= unsafePerformIO $ newIORef $ (0::Int)
-maxtries=  3 * (M.size $ unsafePerformIO $ readIORef rexprs)
+data Tries= Tries Int Int deriving Typeable
+--rtries= unsafePerformIO $ newIORef $ (0::Int)
+--maxtries=  3 * (M.size $ unsafePerformIO $ readIORef rexprs)
 
-rexprs :: IORef (M.Map JSString (Expr Double))
-rexprs= unsafePerformIO $ newIORef M.empty      -- initial expressions
+newtype Exprs= Exprs (M.Map JSString (Expr Double))
+--rexprs :: IORef (M.Map JSString (Expr Double))
+--rexprs= unsafePerformIO $ newIORef M.empty      -- initial expressions
 
-rvars :: IORef (M.Map JSString (Expr Double))
-rvars= unsafePerformIO $ newIORef M.empty        -- expressions actually used for each cell.
+newtype Vars= Vars (M.Map JSString (Expr Double))
+--rvars :: IORef (M.Map JSString (Expr Double))
+--rvars= unsafePerformIO $ newIORef M.empty       -- expressions actually used for each cell.
                                                 -- initially, A mix of reexprs and rmodified
                                                 -- and also contains the result of calculation
 
-rmodified :: IORef (M.Map JSString (Expr Double))
-rmodified= unsafePerformIO $ newIORef M.empty    -- cells modified by the user or by the loop detection mechanism
+newtype Modified= Modified (M.Map JSString (Expr Double)) deriving Typeable
+--rmodified :: IORef (M.Map JSString (Expr Double))
+--rmodified= unsafePerformIO $ newIORef M.empty    -- cells modified by the user or by the loop detection mechanism
 
 
 -- | make a spreadsheet cell. a spreadsheet cell is an input-output box that takes input values from
@@ -177,30 +180,23 @@ rmodified= unsafePerformIO $ newIORef M.empty    -- cells modified by the user o
 mkscell :: JSString -> Maybe Double -> Expr Double -> Widget Double
 mkscell name val expr= mk (scell name expr) val
 
-both mx= local $ runCloud mx Internals.<** runCloud ( atRemote mx)
-
-
+--both mx= local $ runCloud mx   <** runCloud ( atRemote (clustered $ mx >> empty :: Cloud()))
 
 
 scell :: JSString -> Expr Double -> Cell Double
-scell id  expr= Cell{ mk= \mv->  Widget $ runCloud $ do
-                           both $ lliftIO $ do
-                             exprs <- readIORef rexprs
-                             writeIORef rexprs $ M.insert id expr exprs
+scell id  expr= Cell{ mk= \mv -> Widget $  do
+                           Exprs exprs <- getSData <|> return (Exprs M.empty) -- readIORef rexprs
+                           setData . Exprs $ M.insert id expr exprs
 
-                           r <- local $ norender $ getParam (Just id) "text"  mv `fire` OnKeyUp
+                           r <- norender $ getParam (Just id) "text"  mv `fire` OnKeyUp
 
-                           both $ lliftIO $  do
-                               mod <-  readIORef rmodified
-                               writeIORef rmodified  $ M.insert  id (return  r)  mod
+                           Modified mod <-  getSData <|>  return(Modified M.empty)
+                           setData . Modified  $ M.insert  id (return  r)  mod
                            return r
-                       --  `continuePerch`  id
 
+                    , setter= \x -> withElem id $ \e -> setProp e "value" (toJSString $ show1 x)
 
-
-                     , setter= \x -> withElem id $ \e -> setProp e "value" (toJSString $ show1 x)
-
-                     , getter= getit id}
+                    , getter= getID id}
 
 
 
@@ -212,57 +208,58 @@ scell id  expr= Cell{ mk= \mv->  Widget $ runCloud $ do
 calc :: Widget ()
 calc= Widget $  do
   st <- getCont
-  liftIO  $ handle (removeVar st) $ run' st $  do
-          nvs <- liftIO $ readIORef rmodified
+  return() `onBack` (\(e::Loop) -> do removeVar st e; forward Loop !> "ONBACK")
 
-          when (not $ M.null nvs) $ do
+  Modified nvs <- getSData  <|> error "no modified" -- liftIO $ readIORef rmodified
+
+  when (not $ M.null nvs) $ do
             values <-  calc1
             mapM_ (\(n,v) -> boxCell n .= v)  values
-          liftIO $ writeIORef rmodified M.empty
---   return ()
+
+--  liftIO $ writeIORef rmodified M.empty
+
   where
-  run' st x=  Internals.runTransState st x >> return ()
-
-
-  checktries x= unsafePerformIO $ do
-         n <-  readIORef rtries
-         if (n> maxtries) then  error "loop"
-                          else writeIORef rtries $ n+1
+  run' st x=  runTransState st x >> return ()
 
 
   calc1  :: TransIO [(JSString,Double)]
   calc1= do
-    liftIO $ writeIORef rtries 0
-    cells <- liftIO $ readIORef rexprs
-    nvs   <- liftIO $ readIORef rmodified
-    liftIO $ writeIORef rvars $ M.union nvs cells
+    setData $ Tries 0 -- liftIO $ writeIORef rtries 0
+    Exprs cells    <- getSData <|> error "no exprs" -- liftIO $ readIORef rexprs
+    Modified nvs   <- getSData <|> error "mo modified2" -- liftIO $ readIORef rmodified
+    setData . Vars $ M.union nvs cells
     solve
 
+--solve  :: M.Map JSString (Widget a) -> Widget (M.Map JSString a)
+solve :: TransIO [(JSString,Double)]
+solve = do
+     Vars vars <- getSData <|> error "no vars" --  liftIO $ readIORef rvars
+     mapM (solve1 vars) $ M.toList vars
+     where
+
+     solve1 vars (k,f)= do
+        x <- f
+        setData . Vars $ M.insert k (return x) vars
+        return (k,x) :: TransIO (JSString,Double)
 
 
-
-  circular n= "loop detected in cell: "++ show n  ++ " please fix the error"
 
 --  removeVar :: EventF -> SomeException -> IO () -- [(JSString,Double)]
-  removeVar st  = \(e:: Loop) -> handle (removeVar st) $ do
-
-
-    nvs <- readIORef rmodified
-    exprs <- readIORef rexprs
+removeVar st  = \(e:: Loop) ->  do -- runCloud $ both $ localIO $ do
+    Modified nvs <- getSData <|>  error "no modified 3"-- readIORef rmodified
+    Exprs exprs  <- getSData <|>  error " no Exprs2" --readIORef rexprs
 
     case  M.keys exprs \\ M.keys nvs of
-      [] -> do
-
-         error "no more input variables"
+      [] -> error "non solvable circularity in cell dependencies"
       (name:_) -> do
-         mv <-  getit name
+         mv <- liftIO $ getID name
 
          case mv of
             Nothing -> return ()
             Just v  -> do
-                writeIORef rmodified  $ M.insert name ( return v) nvs
-                return ()
-                Internals.runTransState st (norender calc)
+                setData . Modified  $ M.insert name ( return v) nvs
+                return ()  !> ("using",v)
+                norender calc -- runTransState st (norender calc)
                 return ()
 
   -- http://blog.sigfpe.com/2006/11/from-l-theorem-to-spreadsheet.html
@@ -276,19 +273,9 @@ calc= Widget $  do
 
 
 
-
 --loeb x=  map (\f ->  f (loeb  x)) x
 
---solve  :: M.Map JSString (Widget a) -> Widget (M.Map JSString a)
-solve :: TransIO [(JSString,Double)]
-solve = do
-     vars <- liftIO $ readIORef rvars
-     mapM (solve1 vars) $ M.toList vars
-     where
-     solve1 vars (k,f)= do
-        x <- f
-        liftIO $ writeIORef rvars $ M.insert k (return x) vars
-        return (k,x)
+
 
 
 
